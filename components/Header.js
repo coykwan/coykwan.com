54d814c3af80b777e907e1855ddc593171aa7331
Coy Kwan <coykwan@gmail.com>
2019-09-12 07:35:47 -0500

    Added icons and centered content field

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,23 @@
 import Link from "next/link";
-
+// import { SocialIcon } from 'react-social-icons';
 import "./Header.scss";
+import Icon from '@mdi/react'
+import { mdiAccount, mdiGithubCircle, mdiGmail, mdiLinkedin} from '@mdi/js'
 
 const Header = props => (
-  <Link href="/">
-    <div className="Header">{props.appTitle}</div>
-  </Link>
+  <div className="HeaderWrapper"> 
+    <div className="HeaderContent">
+      <Link href="/">
+        <div className="HeaderTitle">{props.appTitle}</div>
+      </Link>
+      <div className="HeaderIcons">
+        <Icon className="HeaderIcon" path={mdiGithubCircle} size={1.5} color="white"/>
+        <Icon className="HeaderIcon" path={mdiGmail} size={1.5} color="white"/>
+        <Icon className="HeaderIcon" path={mdiLinkedin} size={1.5} color="white"/>
+      </div>
+    </div>
+  </div>
+  
 );
 
 export default Header;
